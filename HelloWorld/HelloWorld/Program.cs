aff5f017b82157f79fd6efbefe8efc3bd302afda
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-06 10:48:59 -0300

    Funções

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,5 +2,12 @@
 
 class Program
 {
+    static void Main()
+    {
+        Console.WriteLine("Olá, mundooooo!");
 
+        Console.WriteLine("Eu sou Felipe");
+
+        Console.WriteLine("Eu sou o Goku");
+    }
 }
