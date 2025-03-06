6a6f2e1a50cf277a27ccabc89a37246c7a695330
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-06 11:18:07 -0300

    Modificadores de acesso

--- HelloWorld/HelloWorld/Carro.cs
+++ HelloWorld/HelloWorld/Carro.cs
@@ -11,4 +11,16 @@ class Carro
     {
         Console.WriteLine("Carro DESLIGADO");
     }
+
+    private void Teste1()
+    {
+        Console.WriteLine("Teste 1");
+    }
+
+    internal void Teste2()
+    {
+        Teste1();
+
+        Console.WriteLine("Teste 2");
+    }
 }
