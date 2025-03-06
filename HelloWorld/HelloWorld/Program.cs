f6c6fade0941818e2b10e4bc2cf4374c291e7bcd
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-06 13:25:55 -0300

    Tipo booleano

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,12 +6,7 @@ class Program
 {
     static void Main()
     {
-        double numero1 = 3.14;
-        float numero2 = 3.14f;
-        decimal numero3 = 3.14m;
-
-        int numero4 = 1_000;
-
-        Console.WriteLine(numero4);
+        bool ativo = false;
+        bool ativo2 = true;
     }
 }
