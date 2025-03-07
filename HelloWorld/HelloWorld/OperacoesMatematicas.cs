ae4399dbddcd208f7449ef7313ea233d181aea3d
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-07 14:55:07 -0300

    Static

--- HelloWorld/HelloWorld/OperacoesMatematicas.cs
+++ HelloWorld/HelloWorld/OperacoesMatematicas.cs
@@ -2,7 +2,7 @@
 
 public class OperacoesMatematicas
 {
-    public int Adicionar(int valor1, int valor2) => valor1 + valor2;
+    public static int Adicionar(int valor1, int valor2) => valor1 + valor2;
     
 
     public (int resultado, string autor) Subtrair(int valor1, int valor2)
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,29 +4,8 @@ class Program
 {
     static void Main()
     {
-        //var carro = new Carro("Lamborghini");
+        var resultado = OperacoesMatematicas.Adicionar(1, 2);
 
-        //carro.NomeDoModelo();
-
-        var carro = new Carro("Lamborghini")
-        {
-            LancadoEm = new DateOnly(2020, 01, 01),
-            Cor = Cor.Amarelo
-        };
-
-        var carro2 = new Carro("Porsche")
-        {
-            LancadoEm = new DateOnly(2019, 01, 01),
-            Cor = Cor.Azul
-        };
-
-        carro.NomeDoModelo();
-        carro2.NomeDoModelo();
-
-        //carro.Modelo = "Lamborghini";
-        //carro.Cor = Cor.Amarelo;
-        //carro.LancadoEm = new DateOnly(2020, 01, 01);
-
-        //carro.NomeDoModelo();
+        Console.WriteLine(resultado);
     }
 }
