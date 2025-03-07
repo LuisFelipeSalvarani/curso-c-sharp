20295dd147031163268c6d5e508ed4101404e356
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-07 17:13:45 -0300

    ForEach

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -5,12 +5,20 @@ class Program
     static void Main()
     {
         var lista = new List<string> { "Felipe", "Luis", "Vitor" };
+        var dicionario = new Dictionary<string, string>();
 
-        Console.WriteLine(lista[0]);
+        dicionario.Add("Nome1", "Felipe");
+        dicionario.Add("Nome2", "Luis");
+        dicionario.Add("Nome3", "Vitor");
 
-        for (int i = 0; i < lista.Count; i++)
+        foreach (var nome in lista)
+            Console.WriteLine(nome);
+        
+
+        foreach (var item in dicionario)
         {
-            Console.WriteLine(lista[i]);
+            Console.WriteLine(item.Key);
+            Console.WriteLine(item.Value);
         }
     }
 }
--- HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955ad1fef8221bef0637608a68630ecf604701be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6707d5a92a22995a7cfe7becc19fdcf082de3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
