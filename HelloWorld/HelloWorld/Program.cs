4b64d617127926b6198ebb5b6797839d860928db
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-07 08:46:15 -0300

    Object

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main()
     {
-        string nome = "Felipe Salvarani";
-        int meuNumero = 13;
-
-        var nome2 = "Felipe Salvarani";
-
-        string meuSobrenome;
-        var variavel = DateTime.Now;
+        object minhaVaraivel = "felipe";
     }
 }
