07dd404f6a911ba50a4d337e381dd70d00026ed1
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-07 16:05:40 -0300

    If, else e else if

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -2,18 +2,72 @@
 
 class Program
 {
+    enum Cores{
+        Vermelho,
+        Azul,
+        Amarelo
+    }
+
     static void Main()
     {
-        int numero = 10;
+        int numero = -10;
+        double saldo = 100.50;
+        bool ativo = true;
+        string autor = "Felipe";
+        List<int> lista = new List<int> { 1, 7};
+        Cores cor = Cores.Azul;
 
-        var meuCarro = new Carro("Porsche")
+        if(numero > 0)
         {
-            Cor = Cor.Vermelho,
-            LancadoEm = new DateOnly(2020, 1, 1)
-        };
+            Console.WriteLine("Este numero é POSITIVO");
+        } else if (numero == 0)
+        {
+            Console.WriteLine("Este numero é NEUTRO");
+        }
+        else
+        {
+            Console.WriteLine("Este numero é NEGATIVO");
+        }
 
-        meuCarro.NomeDoModelo();
+        if(saldo > 100.0)
+        {
+            Console.WriteLine("Este numero é POSITIVO");
+        } else if (saldo == 0.0)
+        {
+            Console.WriteLine("Este numero é NEUTRO");
+        }
+        else
+        {
+            Console.WriteLine("Este numero é NEGATIVO");
+        }
 
-        Console.WriteLine("Oi");
+        if (ativo)
+        {
+            Console.WriteLine("VERDADEIRO");
+        }
+        else
+        {
+            Console.WriteLine("FALSO");
+        }
+
+        if(autor.Equals("Felipe"))
+        {
+            Console.WriteLine("O nome é igual");
+        }
+
+        if(lista.Contains(7))
+        {
+            Console.WriteLine("Contém");
+        }
+
+        if(cor == Cores.Azul)
+        {
+            Console.WriteLine("Cor é igual");
+        }
+
+        if(cor == Cores.Azul || numero >= 0)
+        {
+            Console.WriteLine("Entrou");
+        }
     }
 }
