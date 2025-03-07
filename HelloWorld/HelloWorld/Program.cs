b80ff19d2beb94592942efb6d788e680fbfe9647
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-07 08:54:31 -0300

    Null

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
-        object minhaVaraivel = "felipe";
+        int? idade = null;
+
+        bool informouIdade = idade.HasValue;
+        int minhaIdade = idade.Value;
     }
 }
